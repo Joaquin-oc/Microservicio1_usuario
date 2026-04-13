35b0d4bdfb952686517902d88f1082b2ca455be2
Joaquin-oc <joaquin.ocana@utec.edu.pe>
2026-04-12 21:16:13 -0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from database import engine, Base
-from routes import router
-
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI(title="MS1 - Usuarios")
-
-app.include_router(router)
